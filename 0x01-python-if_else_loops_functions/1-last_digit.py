e4c3935241e5bc0fe66927b4ac5485069768f338
valerc123 <valentina.rua123@gmail.com>
2019-09-11 02:52:18 +0000

    Fix pep8 Last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-ldigit=number%10
+ldigit = number % 10
 start = "Last digit of {:d} is {:d}"
-if number >= 0 :
-        if ldigit > 5 :
-                end = "and is greater than 5"
-        elif ldigit == 0 :
-                end = "and is 0"
-        elif ldigit < 6 :
-                end = "and is less than 6 and not 0"
+if number < 0:
+    number *= -1
+if number >= 0:
+    if ldigit > 5:
+        end = "and is greater than 5"
+    elif ldigit == 0:
+        end = "and is 0"
+    else:
+        end = "and is less than 6 and not 0"
 print(start.format(number, ldigit) + " " + end)
