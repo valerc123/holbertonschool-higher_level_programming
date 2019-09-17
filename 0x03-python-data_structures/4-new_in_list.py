f1a4132cda9f34ba1b3ab0c55225cb5c10fe5115
valerc123 <valentina.rua123@gmail.com>
2019-09-17 20:16:46 +0000

    fixed return in if

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     list_copy = my_list.copy()
     if idx < 0 or idx > len(my_list):
-        return list_copy
+        return my_list
     list_copy[idx] = element
     return list_copy
