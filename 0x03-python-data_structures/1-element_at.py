0f6767ce918dd049c81d40bd3748b60f770b9919
valerc123 <valentina.rua123@gmail.com>
2019-09-17 16:28:17 +0000

    get a element of a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    len_list = len(my_list)
+    if idx > len_list:
+        return None
+    else:
+        return my_list[idx]
