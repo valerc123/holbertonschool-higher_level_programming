cd03f5665224db1467067cc070a5f542db72b0c4
valerc123 <valentina.rua123@gmail.com>
2019-09-17 21:11:53 +0000

    Fix shebang

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env pythondd3
+#!/usr/bin/python3
 def no_c(my_string):
     new_str = ""
     for i in my_string:
         if i != 'c' and i != 'C':
-           new_str += i
+            new_str += i
     return new_str
