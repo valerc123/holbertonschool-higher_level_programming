42f681b137dce595815f30c38a5df5a80beded89
valerc123 <valentina.rua123@gmail.com>
2019-09-18 01:12:48 +0000

    change of position of a variable

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    list_copy = my_list.copy()
-    if idx < 0 or idx > len(my_list):
+  if idx < 0 or idx > len(my_list):
         return my_list
-    list_copy[idx] = element
-    return list_copy
+  list_copy = my_list.copy()
+  list_copy[idx] = element
+  return list_copy
