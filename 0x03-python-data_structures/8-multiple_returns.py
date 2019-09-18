9e1f33372e0781ab33af87765e895f1a0894f525
valerc123 <valentina.rua123@gmail.com>
2019-09-18 01:04:28 +0000

    length and first character of a string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,5 +4,5 @@ def multiple_returns(sentence):
     if len_sen == 0:
         sentence[0] = None
         return 0, None
-    tuple_sent = (len_sen, sentence[0])
-    return tuple_sent
+    else:
+        return (len_sen, sentence[0])
