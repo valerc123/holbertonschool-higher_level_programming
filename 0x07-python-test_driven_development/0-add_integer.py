cd098aac9a05aae1c181df0c7e382679d086b9a3
valerc123 <valentina.rua123@gmail.com>
2019-10-04 18:25:01 +0000

    Function that adds 2 integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
-	if not isinstance(a, (int, float)) 
-		raise TypeError("a must be an integer")
-	elif or not isinstance(b, (int, float)):
-		raise TypeError("b must be an integer")	
-	return a + b
+"""
+This is the addition module
+it adds two integers
+a and b must be first casted
+"""
 
+def add_integer(a, b=98):
+    """
+    a and are integers
+    Returns an integer: the addition of a and b
+    """
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    if not (isinstance(a, int)):
+        raise TypeError("a must be an integer")
+    elif not (isinstance(b, int)):
+        raise TypeError("b must be an integer")
+    return (a + b)
