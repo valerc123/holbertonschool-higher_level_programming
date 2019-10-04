e9769af1cbbbb61c7a0b2e396454ab5458300ed5
valerc123 <valentina.rua123@gmail.com>
2019-10-04 19:47:23 +0000

    divides all elements of a matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+"""
+This is the divide module.
+has a function to divide matrix
+Divide all elements of a matrix
+"""
+
+
+def matrix_divided(matrix, div):
+
+    """
+    matrix must be a list of lists of integers/floats
+    Returns a new matrix
+    """
+    new_matrix = []
+    length = 0
+
+    # if the divisor is neither integer nor float
+    if type(div) is not int and type(div) is not float:
+        raise TypeError('div must be a number')
+    # Matrix must be a list of integers or floats
+    if matrix == [] or matrix == [[]]:
+        raise TypeError('matrix must be a matrix (list of lists) '
+                        'of integers/floats')
+    if not isinstance(matrix[0], list) or not isinstance(matrix[1], list):
+            raise TypeError('matrix must be a matrix (list of lists) '
+                            'of integers/floats')
+    # matrix has to exist, can't be less or equal to 0
+    if len(matrix[0]) <= 0:
+        raise TypeError('matrix must be a matrix (list of lists) '
+                        'of integers/floats')
+
+    # 1. Let's create new matrix with new_row
+    for row in matrix:
+        # Create new_row
+        new_row = []
+        # matrix must be a list
+        if type(matrix) not is list:
+            raise TypeError('matrix must be a matrix (list of lists)  '
+                            'of integers/floats')
+    # 2. Row is empty
+        if length == 0:
+            length = len(row)
+        # Each row must be the same size, TypeError
+        elif len(row) is not length:
+            raise TypeError('Each row of the matrix must have the same size')
+    # 3. Each item must be an integer or float
+        for item in row:
+            if not isinstance(item, (int, float)):
+                raise TypeError('matrix must be a matrix (list of lists) '
+                                'of integers/floats')
+    # 4. add content to the row
+        # All elements will be divided by div and rounded to 2 decimal places.
+            new_row.append(item / div, 2)
+        # Add content to the matrix
+        new_matrix.append(new_row)
+    return new_matrix
