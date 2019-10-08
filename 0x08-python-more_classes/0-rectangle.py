8c68e515cc7c56b954888b7816fb20abe8f31834
valerc123 <valentina.rua123@gmail.com>
2019-10-08 04:57:00 +0000

    fixed pep8

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""Module containing empty Rectangle class
+"""
+
+
 class Rectangle:
+    # Empty Rectangle class
     pass
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
+"""Module containing Rectangle class"""
 
 
 class Rectangle:
+    """Class to create a rectangle"""
     def __init__(self, width=0, height=0):
+        """Initialize a new rectangle with `width` and `height`.
+        """
         self.__width = width
         self.__height = height
 
     # Private instance attribute: width
     @property
     def width(self):
+        """Getter function for `width` attribute.
+        Returns: value of `width` attribute.
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Setter function for `width` attribute.
+        Args:
+            value (int): value to use for `width`.
+        Raises:
+            TypeError: If `value` is not of type int.
+            ValueError: If `value` is less than 0.
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -22,10 +36,20 @@ class Rectangle:
     # Private instance attribute: height
     @property
     def height(self):
+        """Getter function for `height` attribute.
+        Returns: value of `height` attribute.
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Setter function for `height` attribute.
+        Args:
+            value (int): value to use for `height`.
+        Raises:
+            TypeError: If `value` is not of type int.
+            ValueError: If `value` is less than 0.
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -33,10 +57,12 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        # Method to compute area of Rectangle instance
         area = self.__width * self.__height
         return area
 
     def perimeter(self):
+        # Method to compute perimeter length of Rectangle instance.
         if self.__width == 0 or self.__height == 0:
             perimeter = 0
         perimeter = (self.__width + self.__height) * 2
