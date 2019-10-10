285014019ea478579f59cedd2b69463ccdc3f816
valerc123 <valentina.rua123@gmail.com>
2019-10-10 02:49:32 +0000

    added comments of the class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
+"""Module implementing LockedClass class.
+LockedClass is a class that uses the __slots__ attribute to prevent
+dynamic attribute creation.
+"""
 class LockedClass:
     """A locked class can't set attributes other
     than those mentioned as 'first_name'"""
