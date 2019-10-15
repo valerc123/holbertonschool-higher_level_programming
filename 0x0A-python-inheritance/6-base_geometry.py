dc994a881d2618f8f19aa8e7e8da48c5e8c8c8bd
valerc123 <valentina.rua123@gmail.com>
2019-10-15 22:36:20 +0000

    Public method that raise Exception error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+    Module - BaseGeometry
+'''
+
+
+class BaseGeometry:
+    ''' class with public attribute area'''
+    def area(self):
+        ''' Raise error Exception'''
+        raise Exception("area() is not implemented")
