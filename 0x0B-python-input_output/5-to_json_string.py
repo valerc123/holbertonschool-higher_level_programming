ddc5cc1e7675859080479b743d2f3f94070e09c0
valerc123 <valentina.rua123@gmail.com>
2019-10-16 18:00:25 +0000

    Json representation in string

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+'''
+    Module - to_json_string
+'''
+
+
+def to_json_string(my_obj):
+    '''
+    Function that returns the JSON representation of an object (string)
+    '''
+    return json.dumps(my_obj)
