d82aa54000980d23bb1f2e9612002715a39aea3d
valerc123 <valentina.rua123@gmail.com>
2019-10-16 16:49:57 +0000

    Number of lines of a file

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+    Module - number_of_lines
+'''
+
+
+def number_of_lines(filename=""):
+    '''
+    Function that returns the number of lines of a text file
+    '''
+    for lines in filename:
+        print(lines, end="")
