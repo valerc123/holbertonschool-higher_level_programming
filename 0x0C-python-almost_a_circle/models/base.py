10c955b9c9a69b99bca26461afc2b83a9a1d41c5
valerc123 <valentina.rua123@gmail.com>
2019-10-21 23:10:44 +0000

    list of dictionaries to a json string representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
+import json
 
 
 class Base:
     __nb_objects = 0
-
+    """Base class"""
     def __init__(self, id=None):
+        """Initialize the base class"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns the Json string representation of list_dictionaries"""
+        if list_dictionaries is None or []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
