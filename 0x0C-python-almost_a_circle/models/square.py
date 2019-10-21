d2dcc40ef80eb9a78aa10d26a4228b959b628a6e
valerc123 <valentina.rua123@gmail.com>
2019-10-21 22:52:08 +0000

    dictionary representation of squre

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,3 +32,40 @@ class Square(Rectangle):
         """
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update method: update the dictionary (key/value)
+        argument to each attribute
+        Args:
+            args: single pointer to a dictionary: key/value
+            kwargs: dictionary with key/value arguments
+        """
+
+        if args and len(args) > 0:
+            for index, arg in enumerate(args):
+                if index == 0:
+                   # super().__init__(arg)
+                    self.id = arg
+                if index == 1:
+                    self.size = arg
+                if index == 2:
+                    self.x = arg
+                if index == 3:
+                    self.y = arg
+        elif kwargs and kwargs is not None and len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                if key == 'size':
+                    self.size = value
+                if key == 'x':
+                    self.x = value
+                if key == 'y':
+                    self.y = value
+
+    def to_dictionary(self):
+        """
+        Returns the dictionary representation of Square
+        """
+        attr = ['id', 'size', 'x', 'y']
+        return {key: getattr(self, key) for key in attr}
