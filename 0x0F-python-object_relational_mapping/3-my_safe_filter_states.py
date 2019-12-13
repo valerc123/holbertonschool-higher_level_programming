8f4827f53ab547e69595eb5288431236bb449a4b
valerc123 <valentina.rua123@gmail.com>
2019-12-13 20:14:01 +0000

    Filter states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    nameSearch = argv[4]
+
+    db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = '{:s}'".format(nameSearch))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
