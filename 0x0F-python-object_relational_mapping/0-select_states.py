454ab0edccd54981a21b9e626ee702270085bac5
valerc123 <valentina.rua123@gmail.com>
2019-12-14 17:45:20 +0000

    Order by asc

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,11 +8,12 @@ passwd = argv[2]
 database = argv[3]
 
 db = MySQLdb.connect(host='localhost',
+                     port=3306,
                      user=username,
                      passwd=passwd,
                      db=database)
 cursor = db.cursor()
-cursor.execute("SELECT * FROM states")
+cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
 rows = cursor.fetchall()
 for row in rows:
     print (row)
