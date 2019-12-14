44f3ef888d85f7fc2049db29c332df341818e279
valerc123 <valentina.rua123@gmail.com>
2019-12-14 18:51:43 +0000

    filters

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
-
+"""
+Lists all states with a name starting with N (upper N)
+"""
 if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    # Take arguments
     argv = sys.argv
     username = argv[1]
     password = argv[2]
     database = argv[3]
-
+    # Connect to a MySQL server
     db = MySQLdb.connect(host='localhost',
+                         port=3306,
                          user=username,
                          passwd=password,
                          db=database)
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name regexp '^N'")
+    # Run query
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
+    # Fetch all fields
     rows = cursor.fetchall()
+    # Print fields
     for row in rows:
-            print (row)
+            print(row)
