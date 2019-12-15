29756e86a02300429228b4d9ec6091a80121151e
valerc123 <valentina.rua123@gmail.com>
2019-12-15 22:09:31 +0000

    change in query

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+    List all cities from the database hbtn_0e_4_usa
 """
 import MySQLdb
 import sys
@@ -18,8 +19,9 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
     cursor.execute("SELECT cities.state_id, cities.name, states.name "
-                   "FROM cities JOIN states ON states.id = cities.state_id "
-                   "ORDER BY cities.id ASC")
+                   "FROM cities JOIN states "
+                   "ON cities.state_id = states.id "
+                   "ORDER BY cities.id")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
