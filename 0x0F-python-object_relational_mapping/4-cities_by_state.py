8fc2f02434dae20bd2b196da759b4577d54f90b0
valerc123 <valentina.rua123@gmail.com>
2019-12-15 22:16:10 +0000

    change from cities.state_id to cities.id

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,10 +2,10 @@
 """
     List all cities from the database hbtn_0e_4_usa
 """
-import MySQLdb
-import sys
-
 if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
     # Save arguments in variables
     argv = sys.argv
     username = argv[1]
@@ -18,7 +18,7 @@ if __name__ == "__main__":
                          db=database)
 
     cursor = db.cursor()
-    cursor.execute("SELECT cities.state_id, cities.name, states.name "
+    cursor.execute("SELECT cities.id, cities.name, states.name "
                    "FROM cities JOIN states "
                    "ON cities.state_id = states.id "
                    "ORDER BY cities.id")
