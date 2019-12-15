2e438a6d6ed953869dee65a7ded803dea56e9969
valerc123 <valentina.rua123@gmail.com>
2019-12-15 21:25:23 +0000

    syntax error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,29 +2,19 @@
 """
     Displays all values in the states table where name matches the argument
 """
-import MySQLdb
-import sys
-
 if __name__ == "__main__":
-    argv = sys.argv
-    username = argv[1]
-    password = argv[2]
-    database = argv[3]
-    nameSearch = argv[4]
-
+    import MySQLdb
+    import sys
     db = MySQLdb.connect(host='localhost',
-                         user=username,
-                         port=3306,
-                         passwd=password,
-                         db=database)
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         database=sys.argv[3])
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states"
-                   "WHERE name = '{:s}'"
-                   "ORDER BY id ASC".format(nameSearch))
+    cursor.execute("SELECT * FROM states "
+                   "WHERE name LIKE BINARY '{:s}' "
+                   "ORDER BY id ASC".format(sys.argv[4]))
 
     rows = cursor.fetchall()
     for row in rows:
         print(row)
-    cursor.close()
-    db.close()
