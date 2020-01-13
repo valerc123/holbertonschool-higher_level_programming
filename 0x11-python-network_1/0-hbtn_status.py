e21cec036c04d63c44acf4ab6447426eef4591c5
valerc123 <valentina.rua123@gmail.com>
2020-01-13 19:44:20 +0000

    tab

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""
+    Script that fecthes url using urllib
+"""
 import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
         html = r.read()
         print("Body response:")
-        print("- type: {}".format(type(html)))
-        print("- content: {}".format(html))
-        print("- utf8 content: {}".format(html.decode("utf-8")))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
