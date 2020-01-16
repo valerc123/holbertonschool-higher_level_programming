f7492705fb91e68fdf4ddf30ba27ea199b82d086
valerc123 <valentina.rua123@gmail.com>
2020-01-16 20:30:36 +0000

    POST request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Sends a POST request to the passed URL with the email
+    as a parameter, and displays the body of the response
+"""
+
+from urllib import request, parse
+import sys
+
+data = {'email': sys.argv[2]}
+data = parse.urlencode(data)
+data = data.encode('ascii')
+
+with request.urlopen(sys.argv[1], data) as request:
+    req = request.read()
+    print(req)
