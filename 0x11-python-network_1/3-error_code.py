c99966a23d4f18fefc726b278a3feb924bc34df0
valerc123 <valentina.rua123@gmail.com>
2020-01-17 00:44:22 +0000

    add if requirement

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,9 +6,10 @@
 from urllib import error, request
 import sys
 
-try:
-    with request.urlopen(sys.argv[1]) as req:
-        req = req.read().decode('utf-8')
-        print(req)
-except error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as req:
+            req = req.read().decode('utf-8')
+            print(req)
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
