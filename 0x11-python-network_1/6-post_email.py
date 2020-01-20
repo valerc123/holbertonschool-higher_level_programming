a3fa62b6b727653ebf59a92041e4ca7477a31827
valerc123 <valentina.rua123@gmail.com>
2020-01-20 01:26:09 +0000

    Post request

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+"""
+
+from sys import argv
+from requests import post
+
+
+if __name__ == "__main__":
+    res = post(argv[1], data = {'email': argv[2]})
+    print(res.text)
