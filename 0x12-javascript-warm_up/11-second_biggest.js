a78aff8c08158fb475bf41cb23ff5b8221a8ef8a
valerc123 <valentina.rua123@gmail.com>
2020-02-04 22:18:47 +0000

    Semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 if (process.argv.length > 3) {
-process.argv.splice(0, 2);
-const max = Math.max(...process.argv);
-const newArray = process.argv.filter(ele => ele !== max.toString());
-console.log(Math.max(...newArray));
+  process.argv.splice(0, 2);
+  const max = Math.max(...process.argv);
+  const newArray = process.argv.filter(ele => ele !== max.toString());
+  console.log(Math.max(...newArray));
 } else {
   console.log(0);
 }
