4d00826724a3fd7846a39033bb46cd7302c3970b
valerc123 <valentina.rua123@gmail.com>
2020-02-12 00:12:10 +0000

    Completed task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,15 +5,18 @@ const req = require('request');
 
 req(url, (err, res, body) => {
   if (err) console.log(err);
-  let count = 0;
   const taskCompleted = {};
   const obj = JSON.parse(body);
   for (const i in obj) {
+    const task = obj[i];
     const userID = obj[i].userId;
-    const task = obj[i].completed;
-    if (task) {
-      count++;
-      taskCompleted[userID] = count;
+
+    if (task.completed === true) {
+      if (taskCompleted[userID] === undefined) {
+        taskCompleted[userID] = 1;
+      } else {
+        taskCompleted[userID] += 1;
+      }
     }
   }
   console.log(taskCompleted);
