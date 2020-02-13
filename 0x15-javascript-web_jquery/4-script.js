7222b26e19a5923d6c6356229ed443447b9a7fc6
Holberton <holberton@Holbertons-iMac-77.local>
2020-02-13 18:03:52 -0500

    toggle class

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,7 @@
+$('#toggle_header').click(() => { 
+    if ($('header').hasClass('red')) {
+        $('header').toggleClass('green');
+    } else if ($('header').hasClass('green')) {
+        $('header').toggleClass('red');
+    }
+});
